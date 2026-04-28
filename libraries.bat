37f80e53160d17cda5747956f2fe6841dd2b16a6
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-28 18:36:56 -0500

    im going insane

--- libraries.bat
+++ libraries.bat
@@ -3,9 +3,9 @@ haxelib newrepo
 haxelib install lime
 haxelib run lime setup
 
-haxelib install flixel-animate
+haxelib install flixel-animate --skip-dependencies
 
-haxelib git flixel https://github.com/FunkinCrew/flixel de7dee9e54f550fadf7cb15c62ead01f889c3802
+haxelib git flixel https://github.com/FunkinCrew/flixel f7b94eebf7dbb452a929d0c67ab31a9cbd71d3a0
 haxelib git flixel-addons https://github.com/FunkinCrew/flixel-addons de7dee9e54f550fadf7cb15c62ead01f889c3802
 
 haxelib git openfl https://github.com/openfl/openfl 0d93f1d46016f2beea37c48c5682bda5b3e73158
