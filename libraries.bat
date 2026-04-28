af58348f9598241c6155e8e7a167ce47e732f75e
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-28 17:53:50 -0500

    grr, curse u flixel for updating some thingy

--- libraries.bat
+++ libraries.bat
@@ -1,21 +1,29 @@
 haxelib newrepo
-haxelib install flixel
-haxelib git openfl https://github.com/openfl/openfl 0d93f1d46016f2beea37c48c5682bda5b3e73158
-haxelib run lime setup flixel
+
+haxelib install lime
 haxelib run lime setup
-haxelib install flixel-tools
+
+haxelib git flixel https://github.com/FunkinCrew/flixel de7dee9e54f550fadf7cb15c62ead01f889c3802
 haxelib git flixel-addons https://github.com/FunkinCrew/flixel-addons de7dee9e54f550fadf7cb15c62ead01f889c3802
+
+haxelib git openfl https://github.com/openfl/openfl 0d93f1d46016f2beea37c48c5682bda5b3e73158
+
+haxelib install flixel-tools
 haxelib install flixel-ui
+
 haxelib install hscript
 haxelib install newgrounds
+
 haxelib git faxe https://github.com/uhrobots/faxe
+
 haxelib git polymod https://github.com/larsiusprime/polymod.git
+
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
-haxelib git extension-webm https://github.com/KadeDev/extension-webm
+
 haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit.git
 haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
-haxelib run lime rebuild extension-webm windows
-haxelib install actuate 
+
 haxelib git thx.core https://github.com/fponticelli/thx.core 2bf2b992e06159510f595554e6b952e47922f128
 haxelib git thx.semver https://github.com/fponticelli/thx.semver bdb191fe7cf745c02a980749906dbf22719e200b
+
 haxelib install flixel-animate
