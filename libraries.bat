680f27eedf857969a8ecb613755b57544411477a
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-28 18:24:34 -0500

    finally reinstalled libraries

--- libraries.bat
+++ libraries.bat
@@ -16,12 +16,12 @@ haxelib install newgrounds
 
 haxelib git faxe https://github.com/uhrobots/faxe
 
-haxelib git polymod https://github.com/larsiusprime/polymod.git
+haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit.git
+haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
 
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 
-haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit.git
-haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
+haxelib git polymod https://github.com/larsiusprime/polymod.git
 
 haxelib git thx.core https://github.com/fponticelli/thx.core 2bf2b992e06159510f595554e6b952e47922f128
 haxelib git thx.semver https://github.com/fponticelli/thx.semver bdb191fe7cf745c02a980749906dbf22719e200b
