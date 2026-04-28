fb616cc202e20830a041bcf0b6007507e3157526
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-28 07:30:23 -0500

    clean up update begins

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -79,13 +79,6 @@ class TitleState extends MusicBeatState
 
 		super.create();
 
-		// NGio.noLogin(APIStuff.API);
-
-		#if ng
-		var ng:NGio = new NGio(APIStuff.API, APIStuff.EncKey);
-		trace('NEWGROUNDS LOL');
-		#end
-
 		FlxG.save.bind('fu-kit', 'Maki');
 
 		KadeEngineData.initSave();
@@ -291,14 +284,6 @@ class TitleState extends MusicBeatState
 
 		if (pressedEnter && !transitioning && skippedIntro)
 		{
-			#if !switch
-			NGio.unlockMedal(60960);
-
-			// If it's Friday according to da clock
-			if (Date.now().getDay() == 5)
-				NGio.unlockMedal(61034);
-			#end
-
 			titleText.animation.play('press');
 
 			FlxG.camera.flash(FlxColor.WHITE, 1);
