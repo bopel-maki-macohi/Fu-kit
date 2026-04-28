c5593f34cadf6732667cd593c07b705e67d57f7e
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-27 19:57:13 -0500

    save and app shit

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -88,7 +88,7 @@ class TitleState extends MusicBeatState
 		trace('NEWGROUNDS LOL');
 		#end
 
-		FlxG.save.bind('funkin', 'ninjamuffin99');
+		FlxG.save.bind('fu-kit', 'Maki');
 
 		KadeEngineData.initSave();
 
