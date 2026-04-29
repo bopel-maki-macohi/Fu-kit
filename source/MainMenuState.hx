273c21e204d19fd98e1b1bdd714c6d086cc2f77a
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-28 22:55:23 -0500

    i keep getting dfjk and im confused

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -109,13 +109,6 @@ class MainMenuState extends MusicBeatState
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
 
-		// NG.core.calls.event.logEvent('swag').send();
-
-		if (FlxG.save.data.dfjk)
-			controls.setKeyboardScheme(KeyboardScheme.Solo, true);
-		else
-			controls.setKeyboardScheme(KeyboardScheme.Duo(true), true);
-
 		changeItem();
 
 		super.create();
