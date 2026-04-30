dc594c62f62def93434f1c580e8e1b944c8a0bcf
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-29 21:13:45 -0500

    HOLY SHIT I FORGOT ABOUT THE CRASH HANDLER LMAO

--- source/fukit/debug/CrashHandler.hx
+++ source/fukit/debug/CrashHandler.hx
@@ -1,5 +1,7 @@
 package fukit.debug;
 
+import flixel.input.keyboard.FlxKey;
+import flixel.FlxG;
 import openfl.events.UncaughtErrorEvent;
 import openfl.Lib;
 
@@ -7,10 +9,21 @@ class CrashHandler
 {
 	public static function init()
 	{
+		if (FlxG.signals.postUpdate.has(errorKeybind))
+			return;
+
 		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onUncaughtError);
+		
+		FlxG.signals.postUpdate.add(errorKeybind);
 	}
 
-	static function onUncaughtError(event:UncaughtErrorEvent)
+	public static var CRASH_KEY:FlxKey = F1;
+
+	static function errorKeybind()
 	{
+		if (FlxG.keys.anyJustPressed([CRASH_KEY]))
+			throw 'Crash via ${CRASH_KEY.toString()}';
 	}
+
+	static function onUncaughtError(event:UncaughtErrorEvent) {}
 }
