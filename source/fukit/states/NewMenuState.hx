74aece8624080d7fb40ceb1a2a276710e01e98dc
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-30 06:49:22 -0500

    NMS menu list

--- source/fukit/states/NewMenuState.hx
+++ source/fukit/states/NewMenuState.hx
@@ -1,5 +1,6 @@
 package fukit.states;
 
+import fukit.states.ui.MenuList;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.addons.display.FlxBackdrop;
@@ -7,6 +8,8 @@ import flixel.addons.display.FlxBackdrop;
 class NewMenuState extends MusicBeatState
 {
 	public var backdrop:FlxBackdrop;
+
+	public var menuList:MenuList;
 
 	override function create()
 	{
@@ -16,13 +19,24 @@ class NewMenuState extends MusicBeatState
 		add(backdrop);
 		backdrop.scale.set(6, 6);
 		backdrop.velocity.set(32, 32);
-        
-        backdrop.antialiasing = false;
+
+		backdrop.antialiasing = false;
+
+		menuList = new MenuList(Vertical);
+		add(menuList);
+
+		menuList.items.set('Story Mode', null);
+		menuList.items.set('Freeplay', null);
+		menuList.items.set('Options', null);
+		menuList.items.set('Exit', () -> Sys.exit(0));
+
+		menuList.regenItems();
 	}
 
-    override function update(elapsed:Float) {
-        super.update(elapsed);
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
 
-        Global.playMainTheme();
-    }
+		Global.playMainTheme();
+	}
 }
