b1636ee39e5c30f86702ae9359df862d99c6c8c3
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-29 22:59:11 -0500

    scale up + no aa

--- source/fukit/states/NewMenuState.hx
+++ source/fukit/states/NewMenuState.hx
@@ -4,14 +4,17 @@ import flixel.addons.display.FlxBackdrop;
 
 class NewMenuState extends MusicBeatState
 {
-    public var backdrop:FlxBackdrop;
+	public var backdrop:FlxBackdrop;
 
 	override function create()
 	{
 		super.create();
 
-        backdrop = new FlxBackdrop(Paths.image('tile', 'fu-kit'));
-        add(backdrop);
-        backdrop.velocity.set(32, 32);
+		backdrop = new FlxBackdrop(Paths.image('tile', 'fu-kit'));
+		add(backdrop);
+		backdrop.scale.set(6, 6);
+		backdrop.velocity.set(32, 32);
+        
+        backdrop.antialiasing = false;
 	}
 }
