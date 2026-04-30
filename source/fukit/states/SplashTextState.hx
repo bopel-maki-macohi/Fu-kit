4df23f293a0abe0e69b78a608231e33f3033de6c
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-29 22:11:41 -0500

    intro text displayz

--- source/fukit/states/SplashTextState.hx
+++ source/fukit/states/SplashTextState.hx
@@ -1,10 +1,13 @@
 package fukit.states;
 
+import flixel.group.FlxGroup;
 import flixel.FlxG;
 
 class SplashTextState extends MusicBeatState
 {
 	public var wackyList:Array<String> = [];
+
+	public var alphabets:FlxGroup;
 
 	override function create()
 	{
@@ -13,6 +16,9 @@ class SplashTextState extends MusicBeatState
 		var wackyStr = FlxG.random.getObject(CoolUtil.coolTextFile(Paths.txt('introText')));
 		wackyList = wackyStr.split('--');
 		trace('wackyList: $wackyList');
+
+		alphabets = new FlxGroup();
+		add(alphabets);
 
 		Conductor.changeBPM(75);
 
@@ -31,5 +37,55 @@ class SplashTextState extends MusicBeatState
 	function onEnd()
 	{
 		FlxG.switchState(() -> new MainMenuState());
+	}
+
+	var mylastbeat:Int = 0;
+
+	override function beatHit()
+	{
+		super.beatHit();
+
+		if (mylastbeat < curBeat)
+		{
+			trace(mylastbeat);
+
+			function addTextsForLengthAtBeats(len:Int, beats:Array<Int>)
+			{
+				if (wackyList.length == len)
+					for (i => beat in beats)
+					{
+						if (mylastbeat == beat)
+							addText(wackyList[i]);
+					}
+			}
+
+			addTextsForLengthAtBeats(1, [3]);
+			addTextsForLengthAtBeats(2, [0, 3]);
+			addTextsForLengthAtBeats(3, [0, 2, 4]);
+			addTextsForLengthAtBeats(4, [0, 1, 3, 4]);
+			addTextsForLengthAtBeats(5, [0, 1, 2, 3, 5]);
+			addTextsForLengthAtBeats(6, [0, 1, 2, 3, 4, 5]);
+
+			mylastbeat++;
+		}
+	}
+
+	var curTexts:Array<String> = [];
+
+	function addText(text:String)
+	{
+		if (curTexts.contains(text))
+		{
+			trace('no repeats!');
+			return;
+		}
+
+		curTexts.push(text);
+		trace('$text');
+
+		var alphabet:Alphabet = new Alphabet(0, 0, text, true);
+		alphabet.screenCenter();
+		alphabet.y = (alphabets.length * 60) + 200;
+		alphabets.add(alphabet);
 	}
 }
