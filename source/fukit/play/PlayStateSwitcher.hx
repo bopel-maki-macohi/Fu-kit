812912873a606b8d779172f980af43a230d15291
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-01 07:14:27 -0500

    some caching stuff too!

--- source/fukit/play/PlayStateSwitcher.hx
+++ source/fukit/play/PlayStateSwitcher.hx
@@ -1,5 +1,6 @@
 package fukit.play;
 
+import openfl.Assets;
 import fukit.play.components.cutscenes.DialogueCutsceneComponent;
 import flixel.FlxState;
 import flixel.FlxG;
@@ -13,6 +14,15 @@ class PlayStateSwitcher
 		var target:PlayState = new PlayState();
 
 		var curSong:String = PlayState.SONG.song.toLowerCase();
+
+		var player1:Boyfriend = new Boyfriend(0, 0, PlayState.SONG.player1);
+		var player2:Character = new Character(0, 0, PlayState.SONG.player2);
+
+		Assets.cache.setBitmapData(player1.graphic.assetsKey, player1.graphic.bitmap);
+		Assets.cache.setBitmapData(player2.graphic.assetsKey, player2.graphic.bitmap);
+
+		FlxG.sound.cache(Paths.inst(curSong));
+		FlxG.sound.cache(Paths.voices(curSong));
 
 		if (allowCutscenes)
 		{
