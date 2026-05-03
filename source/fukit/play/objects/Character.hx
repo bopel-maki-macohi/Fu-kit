1e2a9471a4726143f37dcacb67e46b986ea97bc9
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-03 15:53:54 -0500

    turn off looping

--- source/fukit/play/objects/Character.hx
+++ source/fukit/play/objects/Character.hx
@@ -141,11 +141,11 @@ class Character extends FukitSprite
 				addByPrefix('singRIGHT', 'GF Right Note');
 				addByPrefix('singUP', 'GF Up Note');
 				addByPrefix('singDOWN', 'GF Down Note');
-				anim.addByIndices('sad', 'gf sad', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], '', 24);
-				anim.addByIndices('danceLeft', 'GF Dancing Beat', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], '', 24);
-				anim.addByIndices('danceRight', 'GF Dancing Beat', [15, 16, 17, 18, 19, 20, 21, 22, 23, 25, 26, 27, 28, 29], '', 24);
-				anim.addByIndices('hairBlow', "GF Dancing Beat Hair blowing", [0, 1, 2, 3], '', 24);
-				anim.addByIndices('hairFall', "GF Dancing Beat Hair Landing", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], '', 24);
+				anim.addByIndices('sad', 'gf sad', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], '', 24, false);
+				anim.addByIndices('danceLeft', 'GF Dancing Beat', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], '', 24, false);
+				anim.addByIndices('danceRight', 'GF Dancing Beat', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], '', 24, false);
+				anim.addByIndices('hairBlow', "GF Dancing Beat Hair blowing", [0, 1, 2, 3], '', 24, false);
+				anim.addByIndices('hairFall', "GF Dancing Beat Hair Landing", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], '', 24, false);
 				addByPrefix('scared', 'GF FEAR');
 
 				playAnim('danceRight');
