d5730473178d006978f89c4d82de35bdf98c6966
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-03 13:17:20 -0500

    the omonus fades a bit

--- source/fukit/play/songs/world2/RMRFSong.hx
+++ source/fukit/play/songs/world2/RMRFSong.hx
@@ -1,5 +1,6 @@
 package fukit.play.songs.world2;
 
+import flixel.tweens.FlxTween;
 import fukit.play.stages.RDZone;
 
 class RMRFSong extends RDSong
@@ -12,10 +13,41 @@ class RMRFSong extends RDSong
 
 		if (game == null)
 		{
-            stage = new RDZone();
+			stage = new RDZone();
 			return;
 		}
 
 		stage = cast(game.stage, RDZone);
+	}
+
+	public var omonusColorTween:FlxTween;
+
+	override function onStepHit(step:Int)
+	{
+		super.onStepHit(step);
+
+		if (game?.curSong != 'rmrf')
+			return;
+
+		switch (step)
+		{
+			case 96: omonusColorTween = FlxTween.color(stage.omonus, (Conductor.crochet / 1000) * (144 / step), 0xFFFFFF, 0x444444);
+		}
+	}
+
+	override function onPause()
+	{
+		super.onPause();
+
+		if (omonusColorTween != null)
+			omonusColorTween.active = false;
+	}
+
+	override function onUnpause()
+	{
+		super.onUnpause();
+
+		if (omonusColorTween != null)
+			omonusColorTween.active = true;
 	}
 }
